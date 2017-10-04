141277563d7afb5637581c7d4034ceba7c8301a5
Apostol <kovalev333@mail.ru>
2017-10-04 23:16:54 +0600

    started wright testHelper all files

--- src/components/comment_list.js
+++ src/components/comment_list.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 const CommentList = (props) => {
+	let list = props.comments.map(comment => <li key={comment}>{comment}</li>);
   return (
-    <ul className="comment-list"></ul>
+    <ul className="comment-list">{list}</ul>
   );
+};
+
+function mapStateToProps(state) {
+	return { comments: state.comments };
 }
 
-export default CommentList;
+export default connect(mapStateToProps)(CommentList);
--- test/actions/actions_test.js
+++ test/actions/actions_test.js
@@ -1,6 +1,6 @@
 import { expect } from '../test_helper'
-import SAVE_COMMENT from '../../src/actions/types'
-import { saveComment } from "../../src/actions"
+import { SAVE_COMMENT } from '../../src/actions/types'
+import { saveComment } from '../../src/actions'
 
 describe('actions', () => {
 	describe('saveComment', () => {
@@ -10,7 +10,8 @@ describe('actions', () => {
 		});
 
 		it('has the correct payload', () => {
-
+			const action = saveComment('new comment');
+			expect(action.payload).to.equal('new comment');
 		});
 	});
 });
